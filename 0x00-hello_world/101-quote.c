0f084c53eda857a524fc02c1532657c27a93c568
marvokmarude <marvellousokorie3@gmail.com>
2022-04-11 09:53:36 -0700

    go

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
+
 #include <unistd.h>
 
 /**
- * main - print quote
  *
- * Description: prints Dora's quote
+ *  * main - Entry point
  *
- * Return: Always 1 (Success)
- */
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
+
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+
+			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+
 	return (1);
+
 }
