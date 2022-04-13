712a915ed7d4115cd9829b77b37691caf699f8d1
marvokmarude <59051230+marvokmarude@users.noreply.github.com>
2022-04-13 05:14:39 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,37 +1,19 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * print_alphabet - function that prints the alphabet, lowercase
- *
- *   * You can only use _putchar twice in your code
- *
- *    * Return: nothing.
- *
- *     */
-
-
+ * print_alphabet - function that prints the alphabet, lowercase
+ * You can only use _putchar twice in your code
+ * Return: nothing.
+ */
 
 void print_alphabet(void)
-
 {
+	int lw = 'a';
 
-		int lw = 'a';
-
-
-
-			while (lw <= 'z')
-
-					{
-
-								_putchar(lw);
-
-										lw += 1;
-
-											}
-
-				_putchar(10);
-
+	while (lw <= 'z')
+	{
+		_putchar(lw);
+		lw += 1;
+	}
+	_putchar(10);
 }
