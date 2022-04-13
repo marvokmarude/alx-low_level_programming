a38b9357ab210cab963cb7b6b2e3ceaeecda111a
marvokmarude <59051230+marvokmarude@users.noreply.github.com>
2022-04-13 05:10:23 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,20 @@
-able File  24 lines (19 sloc)  255 Bytes
-   
 #include "main.h"
-
-
 /**
- * main - Entry point
- *
- * Return: nothing, return void
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
  */
-
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	char putchar[] = "_putchar";
+	int i = 0;
 
-return (0);
+	while (putchar[i] != '\0')
+	{
+		_putchar(putchar[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
