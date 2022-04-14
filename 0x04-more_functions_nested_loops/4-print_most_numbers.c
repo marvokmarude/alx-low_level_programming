3b222ccceef607f9baeee2e22f4533711d45937a
marvokmarude <59051230+marvokmarude@users.noreply.github.com>
2022-04-14 22:04:42 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,39 +1,20 @@
 #include "main.h"
 
-
-
 /**
+ * print_most_numbers - function that prints the numbers, from 0 to 9.
  *
- *  * print_most_numbers - function that prints the numbers, from 0 to 9.
- *
- *   *
- *
- *    * Return: nothing.
- *
- *     */
-
-
+ * Return: nothing.
+ */
 
 void print_most_numbers(void)
-
 {
+	int n = 0;
 
-		int n = 0;
-
-
-
-			while (n < 10)
-
-					{
-
-								if (n != 2 && n != 4)
-
-												_putchar(n + '0');
-
-										n += 1;
-
-											}
-
-				_putchar('\n');
-
+	while (n < 10)
+	{
+		if (n != 2 && n != 4)
+			_putchar(n + '0');
+		n += 1;
+	}
+	_putchar('\n');
 }
