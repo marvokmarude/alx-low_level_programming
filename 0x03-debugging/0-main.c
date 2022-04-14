d51e34d4c238e3485b527ecc5002672c2d39a015
marvokmarude <59051230+marvokmarude@users.noreply.github.com>
2022-04-14 20:40:07 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,35 +1,16 @@
-able File  16 lines (12 sloc)  189 Bytes
-
-   
-
 #include "main.h"
 
-
-
 /**
- *
- *  * main - tests function that prints if integer is positive or negative
- *
- *   * Return: 0
- *
- *    */
-
-
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 int main(void)
-
 {
+	int i;
 
-		int i;
+	i = 0;
+	positive_or_negative(i);
 
-
-
-			i = 0;
-
-				positive_or_negative(i);
-
-
-
-					return (0);
-
+	return (0);
 }
