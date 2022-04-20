faa4c1a410636550745e40a12ce9e4b511ca95bb
marvokmarude <59051230+marvokmarude@users.noreply.github.com>
2022-04-20 20:44:56 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,43 +1,22 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * print_rev - function that prints a string, in reverse
- *
- *   * @s: String to print
- *
- *    * Return: nothing.
- *
- *     */
-
-
+ * print_rev - function that prints a string, in reverse
+ * @s: String to print
+ * Return: nothing.
+ */
 
 void print_rev(char *s)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			while (*(s + i) != '\0')
-
-						i += 1;
-
-				i -= 1;
-
-					while (i >= 0)
-
-							{
-
-										_putchar(*(s + i));
-
-												i--;
-
-													}
-
-						_putchar('\n');
-
+	while (*(s + i) != '\0')
+		i += 1;
+	i -= 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
 }
