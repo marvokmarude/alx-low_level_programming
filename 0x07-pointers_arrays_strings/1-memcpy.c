324365c48955d59e480b8a819842a777eae3bd17
marvokmarude <59051230+marvokmarude@users.noreply.github.com>
2022-04-27 04:10:13 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,47 +1,24 @@
 #include "main.h"
 
-
-
 /**
+ * _memcpy - function that copies memory area.
  *
- *  * _memcpy - function that copies memory area.
- *
- *   *
- *
- *    * @dest: dest positions
- *
- *     * @src: source position
- *
- *      * @n: size of bytes
- *
- *       * Return: char
- *
- *        */
-
-
+ * @dest: dest positions
+ * @src: source position
+ * @n: size of bytes
+ * Return: char
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
-
 {
+	unsigned int i = 0;
+	unsigned int j = 0;
 
-		unsigned int i = 0;
-
-			unsigned int j = 0;
-
-
-
-				while (i < n)
-
-						{
-
-									*(dest + i) = *(src + j);
-
-											i += 1;
-
-													j += 1;
-
-														}
-
-					return (dest);
-
+	while (i < n)
+	{
+		*(dest + i) = *(src + j);
+		i += 1;
+		j += 1;
+	}
+	return (dest);
 }
