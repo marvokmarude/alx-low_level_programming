5826b3191c697a24574e128222c87ec4a38e15c0
marvokmarude <59051230+marvokmarude@users.noreply.github.com>
2022-04-29 02:02:12 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,41 +1,21 @@
 #include "main.h"
 
-
-
 /**
+ * _puts_recursion -  function that prints a string
  *
- *  * _puts_recursion -  function that prints a string
+ * @s: string to receive
  *
- *   *
- *
- *    * @s: string to receive
- *
- *     *
- *
- *      * Return: nothing
- *
- *       */
-
-
+ * Return: nothing
+ */
 
 void _puts_recursion(char *s)
-
 {
-
-		if (*s == '\0')
-
-				{
-
-							_putchar('\n');
-
-									return;
-
-										}
-
-			_putchar(*s);
-
-				s++;
-
-					_puts_recursion(s);
-
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
